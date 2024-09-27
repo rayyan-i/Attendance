e0863c9049ac954315b53b1e0a00c3d80d61f25b
rayyan-i <rayyansiddiqui772@gmail.com>
2024-09-28 00:13:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+var userInput1 = prompt("Total student");
+var userInput2 = prompt("total Present");
+var userInput3 = userInput1 - userInput2;
+console.log("Total absent" , userInput3   )
